8ee3c62694445aedd47132125d55f2b202dbbec0
Leedasamurai <lesegoleemac@gmail.com>
2023-12-16 20:00:20 -0800

    swapp3d

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,31 +8,25 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
 	int temp;
-	int swapped;
+	size_t i, k;
 
 	if (!array || !size)
 		return;
 
-	print_array(array, size);
-
-	for (i = 0; i < size - 1; i++)
+	i = 0;
+	while (i < size)
 	{
-		swapped = 0;
-		for (j = 0; j < size - i - 1; j++)
+		for (k = 0; k < size - 1; k++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[k] > array[k + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-
-				swapped = 1;
+				temp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = temp;
 				print_array(array, size);
 			}
 		}
-		if (!swapped)
-			break;
+		i++;
 	}
 }
