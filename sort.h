e65f771021bffac51b80430af5b5593beb89e7fd
Leedasamurai <lesegoleemac@gmail.com>
2023-12-19 03:47:30 -0800

    ascensort

--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -31,6 +32,7 @@ void swap(int *array, size_t size, int *a, int *b);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+void counting_sort(int *array, size_t size);
 
 
 #endif /* SORT_H */
